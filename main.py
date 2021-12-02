714fbcd158f4b856ae6f064d52ee0ee63ea59810
tomaszplawczyk <tomasz.plawczyk@op.pl>
2021-12-02 09:14:19 +0100

    czwartek

--- main.py
+++ main.py
@@ -3,47 +3,57 @@ import numpy as np
 
 class Position:
     def __init__(self):
-        self.treasure_map = [[4, 0, 0, 1, 2], [1, 2, 5, 0, 0], [4, 0, 3, 4, 3]]
-        self.visited = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
-        self.k =0
+        self.treasure_map = [[4, 0, 0, 1, 2],
+                             [1, 2, 5, 0, 0],
+                             [4, 0, 3, 4, 3]]
+        self.visited = [[0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
+        self.sum=0
 
     def search(self):
+
         for z in range(0,len(self.treasure_map)-1):
             for v in range(0,len(self.treasure_map[z])-1):
                 if self.treasure_map[z][v]!=0:
                      self.visited[z][v] = 1
                      self.treasure_value()
-                     print(self.sum_of_treasures())
+                print(self.sum)
 
 
     def treasure_value(self):
+        self.k = 0
 
+        while self.k < np.max(self.visited):
 
-        while self.k < len(self.visited)*len(self.visited[0]):
             self.k += 1
-            self.make_step(self.k)
+            self.make_step(self.k,self.sum)
 # TODO musze zmienić sposób k
-    def make_step(self, k):
+    def make_step(self, k, sum):
         for i in range(0, len(self.visited) ):
             for j in range(0, len(self.visited[i]) ):
                 if self.visited[i][j] == k:
                     if i > 0 and self.treasure_map[i - 1][j] != 0 and self.visited[i - 1][j] == 0:
                         self.visited[i-1][j ] = k+1
+                        self.sum += self.treasure_map[i-1][j]
                         k+=1
+
 
                     if j > 0 and self.treasure_map[i][j - 1] != 0 and self.visited[i][j - 1] == 0:
                         self.visited[i][j - 1] = k+1
+                        self.sum += self.treasure_map[i][j-1]
                         k += 1
 
                     if i < len(self.visited)-1 and self.treasure_map[i + 1][j] != 0 and self.visited[i + 1][j] == 0:
                         self.visited[i+1][j ] = k+1
+                        self.sum += self.treasure_map[i+1][j]
                         k += 1
 
                     if j < len(self.visited[i])-1 and self.treasure_map[i][j + 1] != 0 and self.visited[i][j + 1] == 0:
                         self.visited[i][j + 1]= k+1
+                        self.sum += self.treasure_map[i][j+1]
                         k += 1
 
-//test
+#test
 #TODO zmienić sposób dodawaninia
     def sum_of_treasures(self):
         sum=0
