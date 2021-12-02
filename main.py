35d4df9f09d9b9dcc3f99be7613c3335c552ddf6
tomaszplawczyk <tomasz.plawczyk@op.pl>
2021-12-02 14:16:39 +0100

    Final

--- main.py
+++ main.py
@@ -3,33 +3,39 @@ import numpy as np
 
 class Position:
     def __init__(self):
-        self.treasure_map = [[4, 0, 0, 1, 2],
-                             [1, 2, 5, 0, 0],
-                             [4, 0, 3, 4, 3]]
+        self.treasure_map = [[3, 0, 0, 1, 2],
+                             [0, 1, 4, 0, 0],
+                             [5, 0, 0, 3, 0]]
         self.visited = [[0, 0, 0, 0, 0],
                         [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
         self.sum=0
+        self.max=0
 
     def search(self):
 
-        for z in range(0,len(self.treasure_map)-1):
-            for v in range(0,len(self.treasure_map[z])-1):
+        for z in range(0,len(self.treasure_map)):
+            for v in range(0,len(self.treasure_map[z])):
                 if self.treasure_map[z][v]!=0:
-                     self.visited[z][v] = 1
-                     self.treasure_value()
-                print(self.sum)
-
+                     if self.visited[z][v]==0:
+                        self.visited[z][v] = 1
+                        self.sum = 0
+                        self.sum = self.treasure_map[z][v]
+                        self.treasure_value()
+                        if self.sum > self.max:
+                            self.max = self.sum
+        print(self.max)
 
     def treasure_value(self):
         self.k = 0
 
-        while self.k < np.max(self.visited):
+
+        while self.k <= np.max(self.visited):
 
             self.k += 1
             self.make_step(self.k,self.sum)
 # TODO musze zmienić sposób k
     def make_step(self, k, sum):
-        for i in range(0, len(self.visited) ):
+        for i in range(0, len(self.visited)):
             for j in range(0, len(self.visited[i]) ):
                 if self.visited[i][j] == k:
                     if i > 0 and self.treasure_map[i - 1][j] != 0 and self.visited[i - 1][j] == 0:
@@ -55,13 +61,6 @@ class Position:
 
 #test
 #TODO zmienić sposób dodawaninia
-    def sum_of_treasures(self):
-        sum=0
-        for i in range(0, len(self.visited) ):
-            for j in range(0, len(self.visited[i]) ):
-                if self.visited[i][j]!=0:
-                    sum+=self.treasure_map[i][j]
-        return sum
 
 
 
